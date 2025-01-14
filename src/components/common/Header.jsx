89e00fd442650aeb438ab955e744f75f061756a8
Yevhen <eugene.shust@gmail.com>
2025-01-14 21:05:02 +0100

    Hidden header for app and admin pages.

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,11 +1,23 @@
+import React from 'react';
+import { useLocation, matchPath } from 'react-router-dom';
 import { Navbar } from './Navbar';
 
 export function Header() {
+    const location = useLocation();
+
+    const excludeNavbarPaths = ['/verses/:id', '/app/:id']; 
+
+    const isExcluded = excludeNavbarPaths.some((pattern) =>
+        matchPath({ path: pattern, exact: true }, location.pathname)
+    );
+
+    if (isExcluded) {
+        return null;
+    }
+
     return (
-        <>
-            <div className="border-b-2 border-r-2 border-l-2 border-[#CACED5] shadow shadow-white mb-4">
-                <Navbar />
-            </div>
-        </>
+        <div className="border-b-2 border-r-2 border-l-2 border-[#CACED5] shadow shadow-white mb-4">
+            <Navbar />
+        </div>
     );
 }
